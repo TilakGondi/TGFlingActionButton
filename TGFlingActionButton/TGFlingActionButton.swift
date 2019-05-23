777823461d093c708e075f7ac5280e6250cfa0b3
Tilakkumar Gondi <tilak.gondi@gmail.com>
2019-05-23 23:30:55 +0530

    Curved Fling action button with support for swipe right to trigger action.

--- TGFlingActionButton/TGFlingActionButton.swift
+++ TGFlingActionButton/TGFlingActionButton.swift
@@ -37,7 +37,7 @@ class TGFlingActionButton: UIButton {
     
     private var swipableView:UILabel?
     private var rightImage:UIImageView?
-    private var progress_line:UIView?
+
     
     private(set) var swipe_direction:Swipe_Direction = .none
     
@@ -60,62 +60,54 @@ class TGFlingActionButton: UIButton {
         }
     }
     
-    @IBInspectable var ProgressLine: UIColor?  {
+    @IBInspectable var FlingColor: UIColor?  {
         didSet {
-            self.progress_line?.backgroundColor = ProgressLine!
+            self.FlingColor = FlingColor!
         }
     }
     
-    @IBInspectable var CornerRadius: CGFloat = 0.0  {
+    @IBInspectable var ImageFlingLayer: UIImage?  {
         didSet {
-            self.layer.cornerRadius = CornerRadius
+            self.ImageFlingLayer = ImageFlingLayer!
         }
     }
     
-    @IBInspectable var SwipableLayerCornerRadius: CGFloat = 0.0   {
-        didSet {
-            swipableView?.layer.cornerRadius = SwipableLayerCornerRadius
-        }
-    }
-    
-    
+  
     
     
     override func draw(_ rect: CGRect) {
         
-        progress_line = UIView.init(frame: CGRect(x: 5, y: (self.frame.size.height/2) - 1, width: self.frame.size.width-10, height: 2))
-        progress_line!.backgroundColor = UIColor.gray
-        self.addSubview(progress_line!)
-        
         if self.swipableView == nil {
             self.addSubview(self.setSwipableLayer())
         }
+        self.clipsToBounds = true
         super.draw(rect)
     }
     
     
     func setSwipableLayer() -> UIView {
+        self.layer.cornerRadius = self.frame.size.height/2
         
-        swipableView = UILabel.init(frame: CGRect(x: 15 , y: (self.frame.size.height/2) - 25, width: 120, height: 50))
+        swipableView = UILabel.init(frame: CGRect(x: 2 , y: 2, width: self.frame.size.height - 4, height: (self.frame.size.height - 4)))
         swipableView?.isUserInteractionEnabled = true
         swipableView?.textAlignment = .left
         swipableView?.tag = 1020
         swipableView?.textColor = self.titleColor(for: UIControl.State.normal)
         swipableView?.backgroundColor = UIColor.orange
-        swipableView?.layer.cornerRadius = SwipableLayerCornerRadius
-        swipableView?.text = "Proceed"
+        swipableView?.layer.cornerRadius = (swipableView?.frame.height)!/2
         swipableView?.font = UIFont.boldSystemFont(ofSize: 16.0)
         swipableView?.clipsToBounds = true
         
         
-        
-        rightImage = UIImageView.init(image: UIImage.init(named: "WhiteRightArrow"))
-        rightImage!.frame = CGRect(x: swipableView!.frame.size.width - 45 , y: 5, width: 40, height: 40)
-        rightImage!.isUserInteractionEnabled = true
-        rightImage!.backgroundColor = UIColor.clear
-        rightImage!.contentMode = UIView.ContentMode.scaleAspectFill
-        rightImage?.clipsToBounds = true
-        swipableView!.addSubview(rightImage!)
+        if (ImageFlingLayer != nil) {
+            rightImage = UIImageView.init(image: ImageFlingLayer)
+            rightImage!.frame = CGRect(x: 2 , y: 2, width: (swipableView?.frame.width)! - 4, height: (swipableView?.frame.width)! - 4)
+            rightImage!.isUserInteractionEnabled = true
+            rightImage!.backgroundColor = UIColor.clear
+            rightImage!.contentMode = UIView.ContentMode.scaleAspectFill
+            rightImage?.clipsToBounds = true
+            swipableView!.addSubview(rightImage!)
+        }
         
         
         panGestureR = UIPanGestureRecognizer.init(target: self, action: #selector(handelPanGesture(panGesture:)))
@@ -137,9 +129,9 @@ class TGFlingActionButton: UIButton {
                 self.setTitle("", for: UIControl.State.normal)
                 if panGesture.state == UIGestureRecognizer.State.ended {
                     let x:Int = Int(translation.x)
-                    if  x > Int(self.frame.size.width-150) {
+                    if  x > Int(self.frame.size.width - self.frame.size.height) {
                         UIView.animate(withDuration: 0.2, animations: {
-                            self.swipableView!.frame = CGRect(x:((self.progress_line?.frame.size.width)!-117), y: (self.frame.size.height/2) - 25, width: (self.swipableView?.frame.size.width)!, height: (self.swipableView?.frame.size.height)!)
+                            self.swipableView!.frame = CGRect(x:((self.frame.size.width)-(self.swipableView?.frame.size.width)! - 2), y: (self.frame.size.height/2) - 25, width: (self.swipableView?.frame.size.width)!, height: (self.frame.size.height - 4))
                             self.swipableView?.backgroundColor = self.FinalStateColor
                         })
                         
@@ -148,57 +140,20 @@ class TGFlingActionButton: UIButton {
                         
                     }else{
                         UIView.animate(withDuration: 0.2, animations: {
-                            self.swipableView!.frame = CGRect(x:15, y: (self.frame.size.height/2) - 25, width: (self.swipableView?.frame.size.width)!, height: (self.swipableView?.frame.size.height)!)
-                            
+                            self.swipableView!.frame = CGRect(x:2, y: (self.swipableView?.frame.origin.y)!, width: (self.swipableView?.frame.size.width)!, height: (self.frame.size.height - 4))
                         })
                     }
                 }else{
-                    if Int(translation.x) > Int(self.frame.size.width-125) {
+                    if Int(translation.x) > Int(self.frame.size.width - self.frame.size.height) {
                         return
                     }
                     UIView.animate(withDuration: 0.0) {
-                        self.swipableView!.frame = CGRect(x: (translation.x) , y: (self.frame.size.height/2) - 25, width: (self.swipableView?.frame.size.width)!, height: (self.swipableView?.frame.size.height)!)
+                        self.swipableView!.frame = CGRect(x: (translation.x) , y: (self.frame.size.height/2) - 25, width: (self.swipableView?.frame.size.width)!, height: (self.frame.size.height - 4))
                     }
                     
                 }
             }
         }
-//        }else{
-//            UIView.animate(withDuration: 0.2, animations: {
-//                self.swipableView!.frame = CGRect(x:15, y: (self.frame.size.height/2) - 25, width: (self.swipableView?.frame.size.width)!, height: (self.swipableView?.frame.size.height)!)
-//                self.swipableView?.backgroundColor = UIColor.orange
-//
-//            })
-//
-//            if self.swipe_direction == .none {
-//                self.swipe_direction = self.getSwipeDirection(translation: translation)
-//            }
-//            if self.swipe_direction == .left{
-//                self.swipableView?.text = ""
-//                if panGesture.state == UIGestureRecognizer.State.ended {
-//                    let x:Int = Int(translation.x)
-//                    if  x < Int(100 - self.frame.size.width) {
-//                        UIView.animate(withDuration: 0.2, animations: {
-//                            self.swipableView!.frame = CGRect(x:(0-self.frame.size.width) + self.frame.size.height, y: 0, width: (self.swipableView?.frame.size.width)!, height: (self.swipableView?.frame.size.height)!)
-//                            self.swipableView?.backgroundColor = self.InitialStateColor
-//                            //                            self.setTitle("START RIDE", for: UIControl.State.normal)
-//                            //                            self.backgroundColor = self.green_color
-//
-//                        })
-//                        self.sendActions(for: .valueChanged)
-//                        self.swipe_direction = .none
-//                    }else{
-//                        UIView.animate(withDuration: 0.2, animations: {
-//                            self.swipableView!.frame = CGRect(x:0, y: 0, width: (self.swipableView?.frame.size.width)!, height: (self.swipableView?.frame.size.height)!)
-//                            self.swipableView?.text = "END RIDE"
-//                        })
-//                    }
-//                }else{
-//                    swipableView!.frame = CGRect(x: translation.x, y: 0, width: (swipableView?.frame.size.width)!, height: (swipableView?.frame.size.height)!)
-//                }
-//            }
-//
-//        }
         
     }
     
--- TGFlingActionButton/ViewController.swift
+++ TGFlingActionButton/ViewController.swift
@@ -14,12 +14,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var fling_button: TGFlingActionButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    //Connect the IBAction method with the action type value changed from storyboard
     @IBAction func flingActionCallback(_ sender: TGFlingActionButton) {
         print(sender.swipe_direction)
-        
+        if sender.swipe_direction == .right {
+            
+            //TO DO: Add the code for actions to be performed once the user swipe the button to right.
+            self.activityIndicator.startAnimating()
+        }
         
     }
     
